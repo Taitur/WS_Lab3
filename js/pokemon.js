ae2bacb121a2aebb9dc74a633ab56accc94d3883
Taitur <taiturhacking@gmail.com>
2025-10-09 19:20:09 +0200

    Pokemonak kargatzen ditu (shiny eta normalak)

--- js/pokemon.js
+++ js/pokemon.js
@@ -38,17 +38,24 @@ class Pokemon {
     let top = parseInt(this.element.style.top);
     let left = parseInt(this.element.style.left);
     if (Pokemon.keys.ArrowUp)
-      this.element.style.top = (top + step) + 'px';
-    if (Pokemon.keys.ArrowDown)
       this.element.style.top = (top - step) + 'px';
+    if (Pokemon.keys.ArrowDown)
+      this.element.style.top = (top + step) + 'px';
     if (Pokemon.keys.ArrowLeft)
-      this.element.style.left = (left + step) + 'px';
-    if (Pokemon.keys.ArrowRight)
       this.element.style.left = (left - step) + 'px';
+    if (Pokemon.keys.ArrowRight)
+      this.element.style.left = (left + step) + 'px';
   }
 } // end of Pokemon class
+window.onload = sortuEtaHasi;
+function sortuEtaHasi() {
+  let botoia = document.getElementById("buscarPokemon");
+  
+  botoia.onclick = cargarJuego;
+}
+  
+  
 
-document.addEventListener('DOMContentLoaded', cargarJuego);
 
 document.addEventListener('keydown', function (event) {
 
@@ -76,15 +83,18 @@ setInterval(moveActivePokemon, 10);
 const pokemonNames = ['pikachu', 'bulbasaur', 'charmander', 'squirtle', 'jigglypuff'];
 
 function cargarJuego() {
-
+  let checkbox = document.getElementById("shiny");
   // llamar a loadImage
   loadImage("https://preview.redd.it/dnlz6c3xni951.jpg?width=1080&crop=smart&auto=webp&s=84af1d3e4e27eddc5c612a7b75244a9886389f77")
-    .then(img => document.body.appendChild(img)).catch (err => console.error(err));
+    .then(img => document.body.appendChild(img)).catch(err => console.error(err));
   pokemonNames.forEach(name => {
     //Solicita al servidor externo la imagen del pokemon correspondiente y genera el pokemon
     const res = fetch("https://pokeapi.co/api/v2/pokemon/" + name).then(response => response.json()).then(data => {
-      const spriteUrl = data.sprites.front_default;
-      new Pokemon(name, spriteUrl);
+      if (!checkbox.checked) {
+        new Pokemon(name, data.sprites.front_default);
+      } else {
+        new Pokemon(name, data.sprites.front_shiny);
+      }
     }).catch(err => console.error(err));
 
   });
