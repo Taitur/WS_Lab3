f6ac1a756bae909c6f01bb83ecd8c8e1489803e0
Taitur <taiturhacking@gmail.com>
2025-10-09 18:44:13 +0200

    intento de Ariketa1 ondo

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,71 +1,71 @@
 class Pokemon {
 
-    static activePokemon = null;
-    static keys = {
-        ArrowUp: false,
-        ArrowDown: false,
-        ArrowLeft: false,
-        ArrowRight: false
-    };
+  static activePokemon = null;
+  static keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false
+  };
 
 
-    constructor(name, sprite) {
-        this.name = name;
-        this.sprite = sprite;
-        this.element = this.createElement();
-        this.addEventListeners();
-    }
-    
-    createElement() {
-      const img = document.createElement('img');
-      img.src = this.sprite;
-      img.style.position = 'absolute';
-      img.style.top = Math.ceil(Math.random()*100) + 'px';
-      img.style.left = Math.ceil(Math.random()*100) + 'px';
-      document.body.appendChild(img);
-      return img;
+  constructor(name, sprite) {
+    this.name = name;
+    this.sprite = sprite;
+    this.element = this.createElement();
+    this.addEventListeners();
+  }
 
-    }
-    
-    addEventListeners() {   
-     	this.element.addEventListener('click', () => {
-            Pokemon.activePokemon = this;
-        });
+  createElement() {
+    const img = document.createElement('img');
+    img.src = this.sprite;
+    img.style.position = 'absolute';
+    img.style.top = Math.ceil(Math.random() * 100) + 'px';
+    img.style.left = Math.ceil(Math.random() * 100) + 'px';
+    document.body.appendChild(img);
+    return img;
 
-    }
-    
-    move(step) { 
-      let top = parseInt(this.element.style.top);
-      let left = parseInt(this.element.style.left);
-      if (Pokemon.keys.ArrowUp) 
-        this.element.style.top = (top + step) + 'px';
-      if (Pokemon.keys.ArrowDown)    
-        this.element.style.top = (top - step) + 'px';
-      if (Pokemon.keys.ArrowLeft)
-        this.element.style.left = (left + step) + 'px';
-      if (Pokemon.keys.ArrowRight) 
-        this.element.style.left = (left - step) + 'px';
-    }
+  }
+
+  addEventListeners() {
+    this.element.addEventListener('click', () => {
+      Pokemon.activePokemon = this;
+    });
+
+  }
+
+  move(step) {
+    let top = parseInt(this.element.style.top);
+    let left = parseInt(this.element.style.left);
+    if (Pokemon.keys.ArrowUp)
+      this.element.style.top = (top + step) + 'px';
+    if (Pokemon.keys.ArrowDown)
+      this.element.style.top = (top - step) + 'px';
+    if (Pokemon.keys.ArrowLeft)
+      this.element.style.left = (left + step) + 'px';
+    if (Pokemon.keys.ArrowRight)
+      this.element.style.left = (left - step) + 'px';
+  }
 } // end of Pokemon class
 
 
 document.addEventListener('keydown', function (event) {
-  
-   Pokemon.keys[event.key] = true;
+
+  Pokemon.keys[event.key] = true;
 
 });
 
 document.addEventListener('keyup', function (event) {
-   
+
   Pokemon.keys[event.key] = false;
 
 });
 
 function moveActivePokemon() {
-   const step = 5;
-    if (Pokemon.activePokemon) {
-        Pokemon.activePokemon.move(step);
-    }
+  const step = 5;
+  if (Pokemon.activePokemon) {
+    Pokemon.activePokemon.move(step);
+  }
 
 }
 
@@ -74,18 +74,25 @@ setInterval(moveActivePokemon, 10);
 // Instantiate Pokémon
 const pokemonNames = ['pikachu', 'bulbasaur', 'charmander', 'squirtle', 'jigglypuff'];
 
-function cargarJuego () {
+function cargarJuego() {
 
   // llamar a loadImage
-
+  loadImage("https://preview.redd.it/dnlz6c3xni951.jpg?width=1080&crop=smart&auto=webp&s=84af1d3e4e27eddc5c612a7b75244a9886389f77")
+    .then(img => document.body.appendChild(img)).catch (err => console.error(err));
   pokemonNames.forEach(name => {
-      //Solicita al servidor externo la imagen del pokemon correspondiente y genera el pokemon
+    //Solicita al servidor externo la imagen del pokemon correspondiente y genera el pokemon
+    fetch("https://pokeapi.co/api/v2/pokemon/" + name).then(response => response.json()).then(irudia => irudia.sprites.front_default)
+
   });
-
-
 
 }
 
-function loadImage (url) {
- /// desarrolla la promesa
+function loadImage(url) {
+  /// desarrolla la promesa
+  return new Promise(resolve => {
+    let irudia = new Image()
+    irudia.src = url
+    irudia.width
+    irudia.height
+  });
 }
